ba5da90f5d6b1d2df44811a61437bdd4391d4939
wedsonlima <wedson.sousa.lima@gmail.com>
2017-01-09 14:14:01 -0300

    adding new responses

--- bot/message.rb
+++ bot/message.rb
@@ -6,20 +6,26 @@ Bot.on :message do |message|
   begin
     case message.text
     when '?' # faz nada
+    when /fuder/i
+      message.reply text: 'Depois de você.'
+    when /a(i|í) dentr?o/i
+      message.reply text: 'Do teu.'
+    when /quem (é|eh) (seu|sua) (pai|mãe)/i
+      message.reply text: 'Acho que vou precisar de um exame de DNA pra isso.'
+    when /biscoito ou bolacha/i, /bolacha ou biscoito/i
+      message.reply text: 'https://www.buzzfeed.com/clarissapassos/o-certo-eh-bolacha'
+    when /n(ã|a)o|nunca|jamais|pr(ó|o)xima|depois|nope/i
+      message.reply text: 'Nem queria mesmo.'
+    when /sim|yes|claro|talvez/i
+      message.reply text: 'Vlw mas tô ocupado. Fica pra próxima.'
+    when /feijoada/i
+      message.reply text: 'Acho que toda quarta tem.'
     when /vlw|valeu|obrigado|obg|show|blz|beleza|thanks/i
       message.reply text: ';-)'
     when /quanto|valore?s?|pre(c|ç)os?/i
       message.reply text: 'R$ 1,10 para alunos.'
       message.reply text: 'R$ 7,00 para professores, funcionários e visitantes.'
       message.reply text: 'Só não sei se vale a pena. ;-)'
-    when /n(ã|a)o|nunca|jamais|pr(ó|o)xima|depois|nope/i
-      message.reply text: 'Nem queria mesmo.'
-    when /sim|yes|claro|talvez/i
-      message.reply text: 'Vlw mas tô ocupado. Fica pra próxima.'
-    when /fuder/i
-      message.reply text: 'Depois de você.'
-    when /a(i|í) dentr?o/i
-      message.reply text: 'Do teu.'
     when /alm(o|u)(ç|c)(o|ar) (hoje|hj)/i, /alm(o|u)(ç|c)(o|ar) de (hoje|hj)/i
       ContentReader.show_menu responder: message, week_day: Date.today, daytime: :almoco
     when /alm(o|u)(ç|c)(o|ar) amanh(ã|a)/i, /alm(o|u)(ç|c)(o|ar) de amanh(ã|a)/i
@@ -29,7 +35,7 @@ Bot.on :message do |message|
     when /jantar? amanh(ã|a)/i, /jantar? de amanh(ã|a)/i
       ContentReader.show_menu responder: message, week_day: Date.today + 1, daytime: :jantar
     when /ontem?/i
-      message.reply text: 'Lembro não, oh. Talez eu saiba pra hoje ou pra amanhã.'
+      message.reply text: 'Lembro não, oh.'
     when /hoje/i
       message.reply attachment: ContentReader.menu_options(text: 'Hoje pra qual horário?')
     when /amanhã?/i
@@ -39,7 +45,7 @@ Bot.on :message do |message|
       message.reply attachment: ContentReader.menu_options(text: 'Quer ir no RU hoje pra fazer o que? (É só clicar)')
     end
   rescue => e
-    message.reply text: 'Deu curto-circuito aqui.'
+    message.reply text: 'Deu curto-circuito aqui. T-T'
     message.reply text: e.message
   end
 end
